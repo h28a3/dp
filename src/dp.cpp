ffabc362d3072babdf0cce76cddddee4ccf72a85
h28a3 <at.h.s_24328@iCloud.com>
2024-10-24 09:09:49 +0900

    Add files via upload

--- src/dp.cpp
+++ src/dp.cpp
@@ -1,1 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n = 3;
+    int s = 11;
+    int a[3] = { 2,5,9 };
+    bool dp[n][s + 1];
+    for (int j = 0;j < s + 1;j++) {
+        dp[0][j] = false;
+    }
+    dp[0][0] = true;
+    dp[0][a[0]] = true;
+    for (int i = 1;i < n;i++) {
+        for (int j = 0;j < s + 1;j++) {
+            dp[i][j] = dp[i - 1][j] || dp[i - 1][j - a[i]];
+        }
+    }
+    for (int i = 0;i < n;i++) {
+        for (int j = 1;j < s + 1;j++) {
+            cout << dp[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
